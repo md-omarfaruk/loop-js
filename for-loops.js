e0242d3c98274855ae60c778d59e1fa08cc0cc32
md-omarfaruk <87226097+md-omarfaruk@users.noreply.github.com>
2021-10-28 09:01:01 +0600

    loop
    loops js

---
+++ for-loops.js
@@ -0,0 +1,5 @@
+var friends = ['hassan', 'hossain', 'jashim', 'habib'];
+for(var i = 0; i < friends.length; i++){
+    var hscFrnd = friends[i];
+    console.log(hscFrnd);
+}
---
+++ while-loops.js
@@ -0,0 +1,21 @@
+var number = 10;
+while(number < 15){
+    console.log(number);
+    number = number + 1;
+}
+
+console.log('Md. Omar Faruk');
+
+ var num = 10;
+ while(num < 15){
+     console.log(num);
+     num++;
+ }
+
+console.log('Md. Omar Faruk');
+
+ var yes = 12;
+ while(yes <= 15){
+     console.log(yes);
+     yes++;
+ }
